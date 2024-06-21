24905c3514b1e91b22fb2559940559ba614b3137
Sviatlana Lukashonak <fireflysvet@gmail.com>
2024-06-21 16:53:21 +0300

    test for interface with different languages

---
+++ test_items.py
@@ -0,0 +1,9 @@
+from selenium.webdriver.common.by import By
+
+link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
+
+def test_add_to_cart(browser):
+    browser.get(link)
+    button = browser.find_element(By.CLASS_NAME, "btn-add-to-basket")
+    assert button, "Add to card button isn't found"
+    print(button)
